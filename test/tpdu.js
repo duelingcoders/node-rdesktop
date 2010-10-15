a4144661d16fcb8043d770a913967d7119451209
James Halliday <substack@gmail.com>
2010-10-15 23:32:30 +0000

    test all tpdu codes, passes

--- test/tpdu.js
+++ test/tpdu.js
@@ -1,13 +1,17 @@
 var tpdu = require('rdesktop/tpdu');
+var Hash = require('traverse/hash');
 
-exports.CR = function (assert) {
-    var pat = '1110xxxx';
-    var begin = parseInt(pat.replace(/x/g,'0'), 2);
-    var end = parseInt(pat.replace(/x/g,'1'), 2);
-    for (var i = begin; i < end; i++) {
-        var code = tpdu.fromByte(i)
-        assert.equal(code, 'CR');
-        var b = tpdu.toByte(code, i - begin);
-        assert.equal(b, i);
-    }
+exports.codes = function (assert) {
+    Hash(tpdu.codes).forEach(function (patterns, code) {
+        patterns.forEach(function (pat) {
+            var begin = parseInt(pat.replace(/x/g,'0'), 2);
+            var end = parseInt(pat.replace(/x/g,'1'), 2);
+            for (var i = begin; i <= end; i++) {
+                var c = tpdu.fromByte(i)
+                assert.equal(code, code);
+                var b = tpdu.toByte(code, i - begin);
+                if (code != 'NA') assert.equal(b, i);
+            }
+        })
+    });
 };
