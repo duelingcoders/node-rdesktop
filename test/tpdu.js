a362794654159b05a2c7d56c0df2ab6266a2ce3d
James Halliday <substack@gmail.com>
2010-10-16 07:10:37 +0000

    fromByte => toCode, moved tpdu fields ot of tpkt

--- test/tpdu.js
+++ test/tpdu.js
@@ -7,7 +7,7 @@ exports.codes = function (assert) {
             var begin = parseInt(pat.replace(/x/g,'0'), 2);
             var end = parseInt(pat.replace(/x/g,'1'), 2);
             for (var i = begin; i <= end; i++) {
-                var c = tpdu.fromByte(i)
+                var c = tpdu.toCode(i)
                 assert.equal(code, code);
                 var b = tpdu.toByte(code, i - begin);
                 if (code != 'NA') assert.equal(b, i);
